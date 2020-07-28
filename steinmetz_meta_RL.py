71cfd3b9f1cee94086285689b51fcc31b48aede0
MOBT-ctx <58266572+MOBT-ctx@users.noreply.github.com>
2020-07-28 19:32:19 +0100

    Update steinmetz_meta_RL.py

--- steinmetz_meta_RL.py
+++ steinmetz_meta_RL.py
@@ -80,7 +80,7 @@ class RL:
         #we are basically maximising lik, the model's probability for choosing the action that the mouse chose
         lik = mod_action[mouse_act]
         # minimum of 1e-7 because log(0) is bad
-        logLik = np.log(max(lik,1e-7))
+        logLik = np.log(max(lik,1e-5))
         #print(logLik)
         self.logLik.append(logLik)    
         self.model_action.append(np.random.choice(3,p=mod_action))
@@ -225,8 +225,8 @@ def testParamsWS(params,dat):
       #print(params)
       wsls.run_model()
       errs.append(sum([i!=j for i,j in zip(wsls.mod_acts,wsls.mouse_action)])/wsls.elen)
-    return np.mean(errs)
-    #return -np.sum(np.log(wsls.liks)) # neg lok lik
+    #return np.mean(errs)
+    return -np.sum(np.log(wsls.liks)) # neg lok lik
 
 #TODO need to update to get continues values to minimise (eg, repeat prob)
 accsWS = []
@@ -282,7 +282,7 @@ class META:
         prev_act = self.mouse_action[i]
         curr_act = self.mouse_action[i+1]
       ws_act,ws_lik = self.wsls.choose_action(prev_act,prev_r,curr_act)
-      ws_probs = wsls.a_probs
+      ws_probs = self.wsls.a_probs
       mouse_state = self.rl.condition[i]
       rl_probs = self.rl.choose_action(mouse_state) 
       mouse_act = int(self.mouse_action[i])
@@ -302,11 +302,7 @@ def testParamsMeta(params,dat):
       meta = META(dat,pWS,pLS,pWSFin,pLSFin,thetaW,thetaL,alpha,gamma,kws)
       #print(params)
       meta.run_model()
-      err = sum([i!=j for i,j in zip(meta.mod_acts,meta.mouse_action)])/meta.elen
-      errs.append(err)
-    meanErr = np.mean(errs) #sum or mean
-    return meanErr # 
-    #return -np.sum(np.log(meta.liks)) # neg lok lik
+    return -np.sum(np.log(meta.liks)) # neg lok lik
 
 #TODO time varying param for kws? or just try HMM?
 # what should determine HMM belief - previous mouse behaviour?
@@ -333,7 +329,14 @@ for i in alldat:
   accsMeta.append(accuracy)
   paramsMeta.append(res.x)
   nLLMeta.append(res.fun)
- 
+
+bics = []
+it = 0
+for i in nLLMeta:
+    bics.append(2* i + len(paramsMeta[0]) * np.log(len(alldat[it]['response'])))
+    it+=1
+
+#np.save("paramsMeta.npy",paramsMeta)
 
 print(accsWS)
 print(paramsWS)
@@ -344,58 +347,333 @@ plt.bar(x = np.arange(len(accsWS)),height=accsWS,alpha=0.5,color = 'yellow')
 plt.xlabel('session')
 plt.ylabel('proportion correct model choices')
 
-dat = alldat[0]
-response = dat['response'] #+1 # to make it 0,1,2 rather than -1,0,1
-feedb = np.array(dat['feedback_type'])
-con_right = dat['contrast_right']
-con_left = dat['contrast_left']
-#need to calculate what correct action actually was
-correct_acts = []
-for i in range(len(feedb)):
-    if con_right[i] == 0 and con_left[i] == 0:
-        correct_acts.append(0)
-    elif con_right[i] > con_left[i]:
-        correct_acts.append(-1)
-    elif con_right[i] < con_left[i]:
-        correct_acts.append(1)
-    else: # equal contrast - maybe remove
-        correct_acts.append(np.random.choice([-1,1]))
-correct_acts = np.array(correct_acts) + 1 #to make it 0,1,2 rather than -1,0,1
+testMouseNum = 0
 
-testPs = paramsMeta[0]
-testAccs = []
-correct = []
-meanSame = []
-for i in range(100):
-    tmeta = META(dat,testPs[0],testPs[1],testPs[2],testPs[3],testPs[4],testPs[5],testPs[6],testPs[7],testPs[8])
-    tmeta.run_model()
-    correct.append([p==j for p,j in zip(tmeta.mod_acts,correct_acts)])
-    testAccs.append(([p==j for p,j in zip(tmeta.mod_acts,tmeta.mouse_action)]))
-meanAcc = np.mean(correct,axis=0)
-meanSame = np.mean(testAccs,axis=0)
 
-#plotting mouse accuracy over time
+dat = alldat[2]
+dat['mouse_name']
+dat['date_exp']
 
-accuracy = np.zeros(len(feedb))
-accuracy_rm = np.zeros(len(feedb))
-mod_acc_rm = np.zeros(len(feedb))
-mean_same_rm = np.zeros(len(feedb))
-window = 50
-for i in range(len(feedb)):
-    #get rid of same level contrast trials with 50% reward chance regardless of left or right
-    if feedb[i] == 1:# and (con_right[i] != con_left[i] or con_right[i] == 0): #allow a no-go trial
-        accuracy[i] = 1.
-    else: #elif (con_right[i] != con_left[i] or con_right[i] == 0):
-        accuracy[i] = 0.
-    # Accuracy running mean
-    accuracy_rm[i] = np.mean(accuracy[max(0,i-window):i])
-    mod_acc_rm[i] = np.mean(meanAcc[max(0,i-window):i])
-    mean_same_rm[i] = np.mean(meanSame[max(0,i-window):i])
-#print(accuracy_rm)
-plt.plot(accuracy_rm)
-plt.plot(mod_acc_rm)
-plt.plot(mean_same_rm)
-plt.xlabel('trials')
-plt.ylabel('running average accuracy - window size = ' + str(window))
-plt.show()
-print(np.mean(testAccs))
+
+#Sensory double softmax model # then Q learning then meta then satiation factor or something?
+class sensoryFunc:
+    def __init__(self, goNoGo,leftRight,gngTheta,lrTheta,noGoBias,rightBias,ngbTheta,rbTheta):
+        self.goNoGo = goNoGo # radnom = 0 : perfect = max 
+        self.leftRight = leftRight # random = 0 : perfect = max
+        self.gngTheta = gngTheta
+        self.lrTheta = lrTheta
+        self.noGoBias = noGoBias
+        self.rightBias = rightBias
+        self.ngbTheta = ngbTheta
+        self.rbTheta = rbTheta
+        
+    def actionProbs(self,contrasts):
+        conDiff = abs(contrasts[0] - contrasts[1])
+        noDiff = 1.0 if (contrasts[0] ==0 and contrasts[1] == 0) else 0
+        if conDiff == 0 and (contrasts[0] ==0 and contrasts[1] == 0):
+            conDiff = contrasts[0]+contrasts[1] #hacky way to make sure to go when both stimuli equal and != 0
+        gngArr = np.array([noDiff, conDiff])
+        gngArr[0] += self.noGoBias
+        gngArr *= self.goNoGo
+        gngArr = [min(max(i,-100),100) for i in gngArr] # hardcoded bounds for time varying parameter # mostly just to avoid inf, rather than injecting inductive bias
+        #print(gngArr)       
+        goOrNot = scipy.special.softmax(gngArr)
+        lrArr = np.array([contrasts[1],contrasts[0]+self.rightBias]) * self.leftRight
+        #print(contrasts[0],contrasts[1],lrArr)
+        lrArr = [min(max(i,-100),100) for i in lrArr] # hardcoded bounds for time varying parameter
+        goRight = scipy.special.softmax(lrArr)
+        #left/stay/right
+        aProbs = np.zeros(3)
+        #set stay prob
+        aProbs[1] = goOrNot[0]
+        #left
+        aProbs[0] = (goRight[0]) * goOrNot[1]
+        #right
+        aProbs[2] = goRight[1] * goOrNot[1]
+        self.goNoGo *= self.gngTheta
+        self.leftRight *= self.lrTheta
+        self.noGoBias += self.ngbTheta 
+        self.rightBias += self.rbTheta 
+        #print(aProbs)
+        return aProbs
+    
+class SensoryQ:
+    def __init__(self,dat,goNoGo,leftRight,gngTheta,lrTheta,noGoBias,rightBias,ngbTheta,rbTheta):
+        self.sens = sensoryFunc(goNoGo,leftRight,gngTheta,lrTheta,noGoBias,rightBias,ngbTheta,rbTheta)
+        self.mouse_action = dat['response'] +1 # to make it 0,1,2 rather than -1,0,1
+        self.mouse_reward = np.array(dat['feedback_type'])
+        self.leftCons = dat['contrast_left']
+        self.rightCons = dat['contrast_right']
+        self.elen = len(self.mouse_action)
+        self.mod_acts = []
+        self.mod_liks = []
+        self.mod_probs = []
+        
+    def run_model(self):
+        for i in range(self.elen):
+          cons = np.array([self.leftCons[i],self.rightCons[i]])
+          sens_probs = self.sens.actionProbs(cons)
+          self.mod_liks.append(sens_probs[int(self.mouse_action[i])])
+          self.mod_probs.append(sens_probs)
+          precisionError = 1 - np.sum(sens_probs) # tiny errors break softmax
+          sens_probs[np.argmax(sens_probs)] += precisionError
+          self.mod_acts.append(np.random.choice([0,1,2],p=sens_probs))
+          
+def testParamsSensQ(params,dat):
+    goNoGo,leftRight,gngTheta,lrTheta,noGoBias,rightBias,ngbTheta,rbTheta = params
+    #print(params)
+    #errs = []
+    for p in range(1):
+        sens = SensoryQ(dat,goNoGo,leftRight,gngTheta,lrTheta,noGoBias,rightBias,ngbTheta,rbTheta)
+        sens.run_model()
+        liks = sens.mod_liks
+        logLiks = [np.log(max(i,1e-5)) for i in liks]
+        #errs.append(sum([i!=j for i,j in zip(sensQ.mod_acts,sensQ.mouse_action)])/sensQ.elen)
+        #print(-np.sum(logLiks))
+    return -np.sum(logLiks) 
+ 
+  
+accsSensQ = []
+paramsSensQ = []
+nLLSensQ = []
+actsQ = []
+#run optimiser
+for i in alldat:
+  accuracy = 0
+  # initial values for params
+  dat = i
+  # bounds = bounds for params during optimisation
+  res = de(testParamsSensQ, bounds=[(1e-1,1e+1),(1e-1,1e+1),(0.8,1.5),(0.8,1.5),(-1e+1,1e+1),(-1e+1,1e+1),(-1.2,1.2),(-1.2,1.2)],args=(dat,)) # , needed in args for tuple
+  print(res)
+  acclist = []
+  for j in range(1):
+    sensQ = SensoryQ(dat,res.x[0],res.x[1],res.x[2],res.x[3],res.x[4],res.x[5],res.x[6],res.x[7])
+    sensQ.run_model()
+    #hacky way of getting accuracy for param settings - will lead to variance in accuracy, but the mean should be correct and unbiased
+    acc = np.mean(sensQ.mod_liks)
+    acts = sensQ.mod_acts
+  actsQ.append(acts)
+  accsSensQ.append(acc)
+  print(acc)
+  paramsSensQ.append(res.x)
+  nLLSensQ.append(res.fun)  
+  np.save("paramsSensQ.npy",paramsSensQ)
+
+
+def genPlots(sessionIndices):
+    for testMouseNum in sessionIndices:
+        
+        dat = alldat[testMouseNum]
+        response = dat['response'] #+1 # to make it 0,1,2 rather than -1,0,1
+        feedb = np.array(dat['feedback_type'])
+        con_right = dat['contrast_right']
+        con_left = dat['contrast_left']
+        #need to calculate what correct action actually was
+        correct_acts = []
+        numEqual = 0
+        for i in range(len(feedb)):
+            if con_right[i] == 0 and con_left[i] == 0:
+                correct_acts.append(0)
+            elif con_right[i] > con_left[i]:
+                correct_acts.append(-1)
+            elif con_right[i] < con_left[i]:
+                correct_acts.append(1)
+            else: # equal contrast - maybe remove
+                correct_acts.append(np.random.choice([-1,1]))
+                numEqual +=1
+        correct_acts = np.array(correct_acts) + 1 #to make it 0,1,2 rather than -1,0,1
+        
+        
+           
+        """   
+        nLL=[]
+        for p in range(len(alldat)):
+            testPs = paramsMeta[p]
+            tmeta = META(dat,testPs[0],testPs[1],testPs[2],testPs[3],testPs[4],testPs[5],testPs[6],testPs[7],testPs[8])
+            tmeta.run_model()
+            correct.append([p==j for p,j in zip(tmeta.mod_acts,correct_acts)])
+            testAccs.append(([p==j for p,j in zip(tmeta.mod_acts,tmeta.mouse_action)]))
+            negSumLogLik = np.sum(np.log(tmeta.liks))
+            nLL.append(negSumLogLik)
+        #meanAcc = np.mean(correct,axis=0)
+        #meanSame = np.mean(testAccs,axis=0)
+        bics = []
+        for i in nLL:
+            bic = -2* i + len(paramsMeta[0]) * np.log(len(tmeta.mod_acts))
+            bics.append(bic)
+        se = np.std(bics,axis=0)/np.sqrt(100)
+        
+        meanBics = np.mean(bics,axis=0)
+        
+        
+        testPs = paramsMeta[testMouseNum]
+        testAccs = []
+        correct = []
+        meanSame = []
+        nLL=[]
+        for i in range(10):
+            tmeta = META(dat,testPs[0],testPs[1],testPs[2],testPs[3],testPs[4],testPs[5],testPs[6],testPs[7],testPs[8])
+            tmeta.run_model()
+            correct.append([p==j for p,j in zip(tmeta.mod_acts,correct_acts)])
+            testAccs.append(([p==j for p,j in zip(tmeta.mod_acts,tmeta.mouse_action)]))
+            negSumLogLik = np.sum(np.log(tmeta.liks))
+            nLL.append(negSumLogLik)
+        meanAcc = np.mean(correct,axis=0)
+        meanSame = np.mean(testAccs,axis=0)
+        #get std err
+        #BIC
+        #bic -2 * LL + numParam * log(nTrials)
+        #avgNSLL = np.mean(nLL,axis=0) # mean bics instead
+        """
+        testPs = paramsSensQ[testMouseNum] 
+        sensQ = SensoryQ(dat,testPs[0],testPs[1],testPs[2],testPs[3],testPs[4],testPs[5],testPs[6],testPs[7])
+        sensQ.run_model()
+        #hacky way of getting accuracy for param settings - will lead to variance in accuracy, but the mean should be correct and unbiased
+        acc = np.mean(sensQ.mod_liks)
+        acts = sensQ.mod_acts
+        meanSame =([p==j for p,j in zip(acts,correct_acts)])
+        probs = sensQ.mod_probs
+        
+        #plotting mouse accuracy over time
+        response = response+1
+        mouse_lefts = []
+        req_lefts = []
+        mouse_rights = []
+        req_rights = []
+        mouse_stay = []
+        req_stay = []
+        sq_lefts = []
+        sq_stays = []
+        sq_rights = []
+        sq_left_rm = np.zeros(len(feedb))
+        sq_stay_rm = np.zeros(len(feedb))
+        sq_right_rm = np.zeros(len(feedb))
+        sensQ_acc_rm = np.zeros(len(feedb))
+        m_left_rm = np.zeros(len(feedb))
+        req_left_rm = np.zeros(len(feedb))
+        m_right_rm = np.zeros(len(feedb))
+        req_right_rm = np.zeros(len(feedb))
+        m_stay_rm = np.zeros(len(feedb))
+        req_stay_rm = np.zeros(len(feedb))
+        corr_act_rm = np.zeros(len(feedb))
+        accuracy = np.zeros(len(feedb))
+        accuracy_rm = np.zeros(len(feedb))
+        mod_acc_rm = np.zeros(len(feedb))
+        mean_same_rm = np.zeros(len(feedb))
+        window = 10
+        for i in range(len(feedb)):
+            #get rid of same level contrast trials with 50% reward chance regardless of left or right
+            if feedb[i] == 1:# and (con_right[i] != con_left[i] or con_right[i] == 0): #allow a no-go trial
+                accuracy[i] = 1.
+            else: #elif (con_right[i] != con_left[i] or con_right[i] == 0):
+                accuracy[i] = 0.
+            # Accuracy running mean
+            if response[i] == 0:
+                mouse_lefts.append(1)
+                mouse_stay.append(0)
+                mouse_rights.append(0)
+            elif response[i] == 1:
+                mouse_lefts.append(0)
+                mouse_stay.append(1)
+                mouse_rights.append(0)
+            elif response[i] == 2:
+                mouse_lefts.append(0)
+                mouse_stay.append(0)
+                mouse_rights.append(1)
+            if correct_acts[i] == 0:
+                req_lefts.append(1)
+                req_stay.append(0)
+                req_rights.append(0)
+            elif correct_acts[i] == 1:
+                req_lefts.append(0)
+                req_stay.append(1)
+                req_rights.append(0)
+            elif correct_acts[i] == 2:
+                req_lefts.append(0)
+                req_stay.append(0)
+                req_rights.append(1)
+            sq_lefts.append(probs[i][0])
+            sq_stays.append(probs[i][1])
+            sq_rights.append(probs[i][2])
+            sq_left_rm[i] = (np.mean(sq_lefts[max(0,i-window):i]))
+            sq_stay_rm[i] = (np.mean(sq_stays[max(0,i-window):i]))
+            sq_right_rm[i] = (np.mean(sq_rights[max(0,i-window):i]))
+            accuracy_rm[i] = np.mean(accuracy[max(0,i-window):i])
+            #mod_acc_rm[i] = np.mean(meanAcc[max(0,i-window):i])
+            sensQ_acc_rm[i] = np.mean(meanSame[max(0,i-window):i])
+            #mean_same_rm[i] = np.mean(meanSame[max(0,i-window):i])
+            m_left_rm[i] = np.mean(mouse_lefts[max(0,i-window):i])
+            req_left_rm[i] = np.mean(req_lefts[max(0,i-window):i])
+            m_right_rm[i] = np.mean(mouse_rights[max(0,i-window):i])
+            req_right_rm[i] = np.mean(req_rights[max(0,i-window):i])
+            m_stay_rm[i] = np.mean(mouse_stay[max(0,i-window):i])
+            req_stay_rm[i] = np.mean(req_stay[max(0,i-window):i])
+        #print(accuracy_rm)
+        plt.plot(accuracy_rm,'magenta',label='mouseAcc')
+        plt.plot(sensQ_acc_rm,'cyan',label='modelAcc')
+        plt.title('running average accuracy')
+        plt.legend()
+        #plt.plot(mod_acc_rm)
+        #plt.plot(mean_same_rm)
+        plt.xlabel('trials')
+        plt.ylabel('running average accuracy: window size = ' + str(window))
+        plt.savefig('accuracy for session ' + str(testMouseNum) + ': mouse name = ' + dat['mouse_name']+'.png', dpi=100)
+        plt.show()
+        
+        plt.plot(m_left_rm,color='magenta',label='mouseLeft')
+        plt.plot(req_left_rm, label = 'taskLeft')
+        plt.plot(sq_left_rm,'cyan',label='modelLeft')
+        plt.legend()
+        plt.title('lefts for session ' + str(testMouseNum))
+        plt.savefig('lefts for session ' + str(testMouseNum) + ': mouse name = ' + dat['mouse_name']+'.png', dpi=100)
+        plt.show()
+        
+        plt.plot(m_stay_rm,color='magenta',label='mouseStay')
+        plt.plot(req_stay_rm,label='taskStay')
+        plt.plot(sq_stay_rm,'cyan',label='modelStay')
+        plt.legend()
+        plt.title('stay for session ' + str(testMouseNum))
+        plt.savefig('rights for session ' + str(testMouseNum) + ': mouse name = ' + dat['mouse_name']+'.png', dpi=100)
+        plt.show()
+        
+        plt.plot(m_right_rm,color='magenta',label='mouseRight')
+        plt.plot(req_right_rm,label='taskRight')
+        plt.plot(sq_right_rm,'cyan',label='modelRight')
+        plt.legend()
+        plt.title('rights for session ' + str(testMouseNum))
+        plt.savefig('stays for session ' + str(testMouseNum) + ': mouse name = ' + dat['mouse_name']+'.png', dpi=100)
+        plt.show()
+
+testPlot = genPlots([29])
+
+
+#bics
+bics = []
+for i in nLLSensQ:
+    bics.append(2* i + len(paramsSensQ[0]) * np.log(len(sensQ.mod_acts)))
+allModBics = np.load('allModBics.npy').tolist()
+randBICS=[]
+for i in alldat:
+    length = len(i['response'])
+    BIC = -2*(sum([np.log(0.33)]*length))
+    randBICS.append(BIC)
+
+ranDiffs = [i-j for i,j in zip(randBICS,bics)]   
+plt.bar(x = np.arange(len(ranDiffs)),height=ranDiffs,alpha=0.5,color = 'cyan')
+
+plt.bar(x = np.arange(len(randBICS)),height=randBICS,alpha=0.5,color = 'cyan')
+plt.bar(x = np.arange(len(bics)),height=bics,alpha=0.5,color = 'magenta')
+plt.xlabel('session')
+plt.ylabel('BICs')
+
+plt.bar(x = np.arange(len(bics)),height=bics,alpha=0.5,color = 'magenta')
+plt.bar(x = np.arange(len(allModBics)),height=allModBics,alpha=0.5,color = 'cyan')
+plt.xlabel('session')
+plt.ylabel('BICs')
+
+bicDiffs = [i-j for i,j in zip(allModBics,bics)]
+plt.bar(x = np.arange(len(bicDiffs)),height=bicDiffs,alpha=0.5)
+plt.xlabel('session')
+plt.ylabel('BICs')
+#next TODO: add history effects that aren't dpendent on reward : HMM?
+    
